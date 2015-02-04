5ac70c9c14607cbfe959f6c17a124c1bf243f870
pipcat <pip@pipcat.com>
2015-02-04 17:42:13 +0100

    Update liveall.py

--- serverssports/liveall.py
+++ serverssports/liveall.py
@@ -17,7 +17,7 @@ DEBUG = config.get_setting("debug")
 def find_url_play(data, headers):
     logger.info("[liveall.py] find_url_play")
 
-    pageurl = scrapertools.find_single_match (data, 'src="(http://liveall.tv/player[^"]+)"')
+    pageurl = scrapertools.find_single_match (data, '["\'](http://liveall.tv/player[^"\']+)"')
     if pageurl == '':
         return ''
 
@@ -25,7 +25,7 @@ def find_url_play(data, headers):
     if (DEBUG): logger.info("data2="+data2)
 
     a,b,c,d,f,v = scrapertools.find_single_match(data2,"var a = ([^;]+);\s*var b = ([^;]+);\s*var c = ([^;]+);\s*var d = ([^;]+);\s*var f = ([^;]+);\s*var v_part = '([^']+)'")
-    fileurl = 'rtmp://%d.%d.%d.%d%s' % (int(a)/15,int(b)/15,int(c)/15,int(d)/15,v)
+    fileurl = 'rtmp://%d.%d.%d.%d%s' % (int(a)/int(f),int(b)/int(f),int(c)/int(f),int(d)/int(f),v)
     url = fileurl + ' swfUrl=http://wds.liveall.tv/jwplayer.flash.swf live=1 timeout=15 swfVfy=1 pageUrl=' + pageurl
     
     return url
